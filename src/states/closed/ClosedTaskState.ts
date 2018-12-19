b82e4c9da1a1e53bdb356a0af4780ba143175c23
MrKriegler <n.kriegler@gmail.com>
2018-12-19 09:22:10 +0200

    Added missing states for tasks

---
+++ src/states/closed/ClosedTaskState.ts
@@ -0,0 +1,16 @@
+import { TaskData, TaskStatus } from '@matilda/src/types';
+import { ITaskStateParent } from '../types';
+import { TaskState } from '../TaskState';
+import {ERRORS, throwError } from '@matilda/lib/common';
+
+export class ClosedTaskState extends TaskState {
+  public constructor(parent: ITaskStateParent,
+                     task: TaskData) {
+    super(parent, task);
+  }
+
+  public async moveTo(targetStatus: TaskStatus, data: TaskData): Promise<TaskState> {
+    throwError(ERRORS.EINVALID, 'You cannot move to any state after the task has been completed');
+    return super.moveTo(targetStatus, data);
+  }
+}
